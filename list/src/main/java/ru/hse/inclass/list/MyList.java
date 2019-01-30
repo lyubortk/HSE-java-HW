6ded085b069080f3a6e35423e48fcb251a133f46
Konstantin Lyubort <lyubortk@gmail.com>
2019-01-30 17:46:26 +0300

    iterator now implements ListIterator

--- list/src/main/java/ru/hse/inclass/list/MyList.java
+++ list/src/main/java/ru/hse/inclass/list/MyList.java
@@ -1,42 +1,65 @@
 package ru.hse.inclass.list;
 
+import java.util.AbstractList;
 import java.util.Iterator;
+import java.util.ListIterator;
 import java.util.NoSuchElementException;
 
 /** A simple doubly linked list.
  * Allows to add objects by one and to iterate through content using {@link Iterator} */
-public class MyList implements Iterable<Object>{
+public class MyList<E>  extends AbstractList<E> implements Iterable<E> {
 
     /** Inner class which represents nodes of list*/
     private class ListNode{
-        private ListNode(Object obj) {
+        private ListNode(E obj) {
             this.obj = obj;
         }
-        private Object obj;
+        private E obj;
         private ListNode prev;
         private ListNode next;
     }
 
     /** {@inheritDoc}
      * Implementation of {@link Iterator} for MyList.*/
-    private class MyListIterator implements Iterator<Object> {
-        private MyListIterator(ListNode firstNode) {
-            nextNode = firstNode;
+    private class MyListIterator implements ListIterator<E> {
+        private MyListIterator(ListNode firstNodeInList) {
+            rightNode = firstNodeInList;
+            leftNode = null;
+            nodeOnPreviousMove = null;
+            index = 0;
         }
 
         /** {@inheritDoc} */
         @Override
         public boolean hasNext() {
-            return nextNode != null;
+            return rightNode != null;
         }
 
         /** {@inheritDoc} */
         @Override
-        public Object next() {
-            if (nextNode != null) {
-                prevNode = nextNode;
-                nextNode = nextNode.next;
-                return prevNode.obj;
+        public boolean hasPrevious() {
+            return leftNode != null;
+        }
+
+        @Override
+        public int nextIndex() {
+            return index;
+        }
+
+        @Override
+        public int previousIndex() {
+            return index-1;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public E next() {
+            if (rightNode != null) {
+                leftNode = rightNode;
+                rightNode = leftNode.next;
+                nodeOnPreviousMove = leftNode;
+                index++;
+                return leftNode.obj;
             } else {
                 throw new NoSuchElementException();
             }
@@ -44,43 +67,74 @@ public class MyList implements Iterable<Object>{
 
         /** {@inheritDoc} */
         @Override
-        public void remove() {
-            if (prevNode != null) {
-                if (prevNode.prev != null) {
-                    prevNode.prev.next = nextNode;
-                } else {
-                    head = nextNode;
-                }
-
-                if (nextNode != null) {
-                    nextNode.prev = prevNode.prev;
-                }
-                prevNode = null;
+        public E previous() {
+            if (leftNode != null) {
+                rightNode = leftNode;
+                leftNode = rightNode.prev;
+                nodeOnPreviousMove = rightNode;
+                index--;
+                return rightNode.obj;
             } else {
-                throw new IllegalStateException();
+                throw new NoSuchElementException();
             }
         }
 
-        private ListNode nextNode;
-        private ListNode prevNode;
+
+        @Override
+        public void add(E e) {
+            var tempNode = new ListNode(e);
+            tempNode.next = rightNode;
+            tempNode.prev = leftNode;
+            if (leftNode != null) {
+                leftNode.next = tempNode;
+            }
+            if (rightNode != null) {
+                rightNode.prev = tempNode;
+            }
+            leftNode = tempNode;
+            index++;
+            nodeOnPreviousMove = null;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public void remove() {
+            if (nodeOnPreviousMove == null) {
+                throw new IllegalStateException();
+            } else {
+                if (nodeOnPreviousMove.prev != null) {
+                    nodeOnPreviousMove.prev.next = nodeOnPreviousMove.next;
+                }
+                if (nodeOnPreviousMove.next != null) {
+                    nodeOnPreviousMove.next.prev = nodeOnPreviousMove.prev;
+                }
+
+                if (leftNode == nodeOnPreviousMove) {
+                    leftNode = nodeOnPreviousMove.prev;
+                    index--;
+                } else {
+                    rightNode = nodeOnPreviousMove.next;
+                }
+
+                nodeOnPreviousMove = null;
+            }
+        }
+
+        @Override
+        public void set(E e) {
+            nodeOnPreviousMove.obj = e;
+        }
+
+        private ListNode rightNode;
+        private ListNode leftNode;
+        private ListNode nodeOnPreviousMove;
+        private int index;
     }
 
     /** {@inheritDoc} */
     @Override
     public MyListIterator iterator() {
         return new MyListIterator(head);
-    }
-
-    /** Inserts one {@link Object} to the front of the list. I.e. iteration
-     * by {@link Iterator} will start with the newest added element.
-     * @param obj an {@link Object} to add to the list */
-    public void insertObject(Object obj) {
-        var node = new ListNode(obj);
-        if (head != null) {
-            node.next = head;
-            head.prev = node;
-        }
-        head = node;
     }
 
     /** A reference to the front node of the list */
