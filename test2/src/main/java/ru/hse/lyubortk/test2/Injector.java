939d08c23623723327efe05015c70eaf73d07071
Konstantin Lyubort <lyubortk@gmail.com>
2019-03-06 16:49:38 +0300

    javadoc added

--- test2/src/main/java/ru/hse/lyubortk/test2/Injector.java
+++ test2/src/main/java/ru/hse/lyubortk/test2/Injector.java
@@ -8,16 +8,38 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-
+/**
+ * Injector class for automatic initialization of object and its dependencies
+ */
 public class Injector {
+    /**
+     * Creates and initializes object of `rootClassName` class using classes from
+     * `implementationClassNames` for concrete dependencies.
+     * @throws ImplementationNotFoundException if implementation isn't found in implementationClassNames
+     * @throws AmbiguousImplementationException if there is multiple implementation which could be
+     * instantiated for object creation
+     * @throws InjectionCycleException if cycle dependency is found
+     * @throws IllegalAccessException if constructors of some argument classes are private
+     * @throws InvocationTargetException if problem with constructor ocured
+     * @throws InstantiationException if problem with constructor ocured
+     */
+    public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
+        Class targetClass = Class.forName(rootClassName);
+        var implementationClasses = new ArrayList<Class>();
+
+        for (var className : implementationClassNames) {
+            implementationClasses.add(Class.forName(className));
+        }
+
+        return dfsCreate(targetClass, implementationClasses, new HashMap<>());
+    }
 
     private static Object dfsCreate(Class target, List<Class> implementations, Map<Class, Object> created)
             throws ImplementationNotFoundException, AmbiguousImplementationException, InjectionCycleException,
             IllegalAccessException, InvocationTargetException, InstantiationException {
         Constructor constructor = target.getDeclaredConstructors()[0];
 
-        Class[] dependencies = constructor.getParameterTypes();
-
+        Class<?>[] dependencies = constructor.getParameterTypes();
         List<Object> createdDependecies = new ArrayList<>();
 
         for (var clazz : dependencies) {
@@ -47,20 +69,5 @@ public class Injector {
         Object resultObject = constructor.newInstance(createdDependecies.toArray());
         created.put(target, resultObject);
         return resultObject;
-    }
-
-    /**
-     * Create and initialize object of `rootClassName` class using classes from
-     * `implementationClassNames` for concrete dependencies.
-     */
-    public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
-        Class targetClass = Class.forName(rootClassName);
-        var implementationClasses = new ArrayList<Class>();
-
-        for (var className : implementationClassNames) {
-            implementationClasses.add(Class.forName(className));
-        }
-
-        return dfsCreate(targetClass, implementationClasses, new HashMap<>());
     }
 }
