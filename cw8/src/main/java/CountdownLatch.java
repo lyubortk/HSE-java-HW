a2c64985fa5733e724fbf72094d35aacff5fba10
Konstantin Lyubort <lyubortk@gmail.com>
2019-04-17 16:52:57 +0300

    implement CountdownLatch

--- cw8/src/main/java/CountdownLatch.java
+++ cw8/src/main/java/CountdownLatch.java
@@ -1,17 +1,49 @@
-public class CountdownLatch {
-    CountdownLatch(int count) {
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+public class CountdownLatch {
+    private final Lock baseLock;
+    private final Condition awaitCondition;
+    private final Condition countdownCondition;
+    private int count;
+
+    CountdownLatch(int count) {
+        if (count < 0) {
+            throw new IllegalArgumentException();
+        }
+        baseLock = new ReentrantLock();
+        awaitCondition = baseLock.newCondition();
+        countdownCondition = baseLock.newCondition();
+        this.count = count;
     }
 
     public void await() {
-
+        baseLock.lock();
+        while (count != 0) {
+            awaitCondition.awaitUninterruptibly();
+        }
+        baseLock.unlock();
     }
 
     public void countDown() {
-
+        baseLock.lock();
+        while (count == 0) {
+            countdownCondition.awaitUninterruptibly();
+        }
+        count--;
+        if (count == 0) {
+            awaitCondition.signalAll();
+        }
+        baseLock.unlock();
     }
 
     public void countUp() {
-
+        baseLock.lock();
+        count++;
+        if (count == 1) {
+            countdownCondition.signal();
+        }
+        baseLock.unlock();
     }
 }
